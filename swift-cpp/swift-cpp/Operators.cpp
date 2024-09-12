0511c4b42966c810f058676b393ce3e62a5e80f6
Tarek <jradi3@gmail.com>
2024-09-12 17:02:58 +0100

    cpp and swiftUI files create

---
+++ swift-cpp/swift-cpp/Operators.cpp
@@ -0,0 +1,8 @@
+//
+//  Operators.cpp
+//  swift-cpp
+//
+//  Created by Tarek Jradi on 12/09/2024.
+//
+
+#include "Operators.hpp"
---
+++ swift-cpp/swift-cpp/Operators.hpp
@@ -0,0 +1,13 @@
+//
+//  Operators.hpp
+//  swift-cpp
+//
+//  Created by Tarek Jradi on 12/09/2024.
+//
+
+#ifndef Operators_hpp
+#define Operators_hpp
+
+#include <stdio.h>
+
+#endif /* Operators_hpp */
